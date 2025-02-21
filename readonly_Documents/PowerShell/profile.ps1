c159d6f7bc1eb03c9998781e8590db934a26812c
Miguel A. Cuevas G. <81328194+secretarybird97@users.noreply.github.com>
2025-02-20 21:10:01 -0800

    feat: Update chezmoiignore to exclude Documents and add PowerShell profile with custom aliases and functions

---
+++ readonly_Documents/PowerShell/profile.ps1
@@ -0,0 +1,11 @@
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+
+Set-Alias ls eza
+
+function l { eza -F --icons --git @args}
+function ll {eza -alF --icons --git @args}
+function la {eza -A --icons --git @args}
+
+Set-Alias vim nvim
+Set-Alias pn pnpm
